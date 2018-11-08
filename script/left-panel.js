1407344406345f4c03d12ca9518e4122cb7eea47
Abnerene <abnerenergy@gmail.com>
2018-11-07 18:45:58 -0600

    Add files via upload

---
+++ script/left-panel.js
@@ -0,0 +1,57 @@
+ 
+      function check1(box) {
+
+   
+      if (box.checked) {
+            document.getElementById("ch-busqueda").checked=false;
+            document.getElementById("ch-calendario").checked=false;
+            document.getElementById("ch-mundo").checked=false;
+            //document.getElementById("i2").src ="icons/calendar.png";
+            //document.getElementById("s3").src ="icons/calendar.png";
+            //document.getElementById("g4").src ="icons/calendar.png";
+      }
+      }
+
+      function check2(box) 
+      {
+       
+
+      if (box.checked) {
+            document.getElementById("ch-usuario").checked=false
+            document.getElementById("ch-calendario").checked=false
+            document.getElementById("ch-mundo").checked=false
+            //document.getElementById("i2").src ="icons/calendar.png";
+            //document.getElementById("s3").src ="icons/calendar.png";
+            //document.getElementById("g4").src ="icons/calendar.png";
+            }
+      }
+
+
+      function check3(box) 
+      {
+      if (box.checked) {
+      document.getElementById("ch-usuario").checked=false
+      document.getElementById("ch-busqueda").checked=false
+      document.getElementById("ch-mundo").checked=false
+      //document.getElementById("i2").src ="icons/calendar.png";
+      //document.getElementById("s3").src ="icons/calendar.png";
+      //document.getElementById("g4").src ="icons/calendar.png";
+      }
+
+      
+      }
+
+      function check4(box) {
+      if (box.checked) {
+      document.getElementById("ch-usuario").checked=false
+      document.getElementById("ch-busqueda").checked=false
+      document.getElementById("ch-calendario").checked=false
+            //document.getElementById("i2").src ="icons/calendar.png";
+            //document.getElementById("s3").src ="icons/calendar.png";
+            //document.getElementById("g4").src ="icons/calendar.png";
+      }
+
+      
+      }
+
+
