df30bc903ec13486b3abdee9bcad9b8c6dc69503
Abnerene <abnerenergy@gmail.com>
2018-11-12 00:34:44 -0600

    Add files via upload

---
+++ script/inicio.js
@@ -0,0 +1,33 @@
+ 
+      function check1(box) {
+
+   
+      if (box.checked) {
+            $('body, html').animate({
+                  scrollTop: '0px'
+            }, 800);
+      }
+ 
+      }
+
+        function check2(box) {
+
+   
+      if (box.checked) {
+            $('body, html').animate({
+                  scrollTop: '0px'
+            }, 800);
+      }
+ 
+      }
+
+       function check3(box) {
+
+   
+      if (box.checked) {
+            $('body, html').animate({
+                  scrollTop: '0px'
+            }, 800);
+      }
+ 
+      }
